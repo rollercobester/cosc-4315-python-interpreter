050f48ac4ae6168c4472f63616b41169acac6ccc
Coby <cobywalters@protonmail.com>
2024-04-12 15:14:11 -0500

    added phase 2 test cases

--- testcases/test.py
+++ testcases/test.py
@@ -1,3 +1,11 @@
-a = False
-b = True
-print(a and (a and not b))
+a = 3
+if a == 3 or a < 1:
+    if a == 4:
+        b = 3
+        a = 5
+    else:
+        a = 2
+    b = 2
+else:
+    a = 1
+print(a)
