c9a334794de5e2739d14f55ca44ae3c99d41b365
Coby Walters <main.neatly207@passmail.net>
2024-04-23 19:32:08 -0500

    new testcases

--- testcases/test.py
+++ testcases/test.py
@@ -1,5 +1,13 @@
-a = 3
-def x(b):
-    b = b + 1
-x(a)
-print(a)
+x = 5
+
+def fn_a():
+    print("x =", x)
+    return x
+
+def fn_b(x):
+    print("x =", x)
+    fn_a()
+    return x
+
+b = fn_b(10)
+print("b =", b)
